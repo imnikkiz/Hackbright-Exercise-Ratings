932bd8e7593c47108ca4e514dce6de43a0cd0003
Hackbright Student <info@hackbrightacademy.com>
2015-02-09 12:56:52 -0800

    First commit

--- seed.py
+++ seed.py
@@ -2,21 +2,59 @@ import model
 import csv
 
 def load_users(session):
-    # use u.user
-    pass
+    user_file = open("./seed_data/u.user")
+    for line in user_file:
+        line = line.strip()
+        line = line.split("|")
+
+
+        user = model.User()
+        user.age = line[1].decode("latin-1")
+        user.zipcode = line[4].decode("latin-1")
+
+
+        session.add(user)
+        
+    session.commit()
 
 def load_movies(session):
-    # use u.item
-    pass
+    movie_file = open("./seed_data/u.item")
+    for line in movie_file:
+        line = line.strip()
+        line = line.split("|")
+        
+
+        movie = model.Movie()
+        movie.title = line[1].decode("latin-1")
+        movie.release_date = line[2].decode("latin-1")
+        movie.imdb_url = line[4].decode("latin-1")
+
+        session.add(movie)
+
+    session.commit()
 
 def load_ratings(session):
-    # use u.data
-    pass
+    rating_file = open("./seed_data/u.data")
+    for line in rating_file:
+        line = line.strip()
+        line = line.split()
+        
+
+        rating = model.Rating()
+        rating.user_id = line[0].decode("latin-1")
+        rating.movie_id = line[1].decode("latin-1")
+        rating.rating = line[2].decode("latin-1")
+
+        session.add(rating)
+
+    session.commit()
+
 
 def main(session):
-    # You'll call each of the load_* functions with the session as an argument
-    pass
+    # load_users(session)
+    # load_movies(session)
+    load_ratings(session)
 
 if __name__ == "__main__":
-    s= model.connect()
+    s = model.connect()
     main(s)
